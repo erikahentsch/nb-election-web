49c4ebb80f60041ddaaa023d6fc65e93ccb6e2cb
Erika Hentsch <erikahentsch@hotmail.com>
2020-07-16 14:47:22 -0400

    Initialized server and React

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const [data, setData] = useState(null)
+  const [loading, toggleLoading] = useState(true)
+
+  useEffect(()=>[
+    getData()
+  ],[])
+
+  const getData = () => {
+    fetch('/flower')
+      .then(res=>res.json())
+      .then(json=>{
+        setData(json);
+        toggleLoading(false)
+      })
+  }
+
+  return (
+    <div className="App">
+      {loading ? "Loading..." :
+        <div>
+          <h1>{data.name}</h1>
+          <p>{data.colour}</p>
+        </div>
+      }
+    </div>
+  );
+}
+
+export default App;
